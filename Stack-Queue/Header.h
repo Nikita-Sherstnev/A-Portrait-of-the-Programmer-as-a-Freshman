69dfe624847cb7da66cca1d97047209415b3f0d2
Nikita-Sherstnev <nikita1248@yandex.ru>
2023-03-12 18:47:23 +0100

    Rewrite stacks and queues to C

--- Stack-Queue/Header.h
+++ Stack-Queue/Header.h
@@ -6,58 +6,59 @@
 #define PF printf
 #define SC scanf
 
-struct stack {
+
+typedef struct stack {
 	int inf;
-	stack *next;
-};
+	struct stack *next;
+} stack;
 
-struct stackw {
-	char word;
-	stackw *next;
-};
+typedef struct stackw {
+    char word;
+    struct stackw *next;
+} stackw;
 
-struct stacks {
-	char inf[15];  
-	stacks *next;
-};
+typedef struct stacks {
+    char inf[15];
+    struct stacks *next;
+} stacks;
 
-struct tqueue {
-	int inf;
-	tqueue *next;
-};
+typedef struct tqueue {
+    int inf;
+    struct tqueue *next;
+} tqueue;
 
-struct tqueuew {
-	char word;
-	tqueuew *next;
-}; 
+typedef struct tqueuew {
+    char word;
+    struct tqueuew *next;
+} tqueuew;
 
-void init_stack(stack *&s);
-void push(stack *&s, int item);
-int pop(stack *&s);
+void init_stack(stack **s);
+void push(stack **s, int item);
+int pop(stack **s);
 int peek(stack *s);
 int empty_stack(stack *s);
 
-void init_stackw(stackw *&s);
-void pushw(stackw *&s, char item);
-int popw(stackw *&s);
+void init_stackw(stackw **s);
+void pushw(stackw **s, char item);
+int popw(stackw **s);
 int peekw(stackw *s);
 int empty_stackw(stackw *s);
 
-void init_queue(tqueue *&h, tqueue *&t);
-void insert(tqueue *&h, tqueue *&t, int item);
-int take(tqueue *&h, tqueue *&t);
-int peek_queue(tqueue *&h, tqueue *&t);
-int empty_queue(tqueue *&h, tqueue *&t);
+void init_queue(tqueue **h, tqueue **t);
+void insert(tqueue **h, tqueue **t, int item);
+int take(tqueue **h, tqueue **t);
+int peek_queue(tqueue **h, tqueue **t);
+int empty_queue(tqueue *h, tqueue *t);
 
-void init_queuew(tqueuew *&h, tqueuew *&t);
-void insertw(tqueuew *&h, tqueuew *&t, char item);
-int takew(tqueuew *&h, tqueuew *&t);
-int peek_queuew(tqueuew *&h, tqueuew *&t);
-int empty_queuew(tqueuew *&h, tqueuew *&t);
+void init_queuew(tqueuew **h, tqueuew **t);
+void insertw(tqueuew **h, tqueuew **t, char item);
+int takew(tqueuew **h, tqueuew **t);
+int peek_queuew(tqueuew **h, tqueuew **t);
+int empty_queuew(tqueuew *h, tqueuew *t);
 
-void init_stacks(stacks *&s);
-void pushs(stacks *&s, char *item);
-char* pops(stacks *&s);
+void init_stacks(stacks **s);
+void pushs(stacks **s, char *item);
+char* pops(stacks **s);
 char* peeks(stacks *s);
 int empty_stacks(stacks *s);
 
---
+++ Stack-Queue/README.md
@@ -0,0 +1,6 @@
+### Compile and run
+
+```
+gcc -finput-charset=CP1251 Func.c StackQueue.c Tasks.c -o program.o
+./program.o
+```
